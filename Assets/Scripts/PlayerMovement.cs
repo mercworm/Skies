a091ead00a7cf16692db0679267142c53465ae8b
Beck <1008887@student.sae.edu.au>
2018-08-13 09:17:03 +1000

    Fixed Player Movement Added a telescope view, added gamemanager, added an eventmanager Player can move around and cannot go outside the borders of the image (just put colliders there)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb2d;
+
+    public GameObject midPoint;
+
+    private void OnEnable()
+    {
+        EventManager.StartListening("ResetPlayerMovement", ResetMovement);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb2d.AddForce(movement * speed);
+    }
+
+    public void ResetMovement ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, midPoint.transform.position, speed);
+    }
+}
