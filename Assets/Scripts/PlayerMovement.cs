574f26ec1c58b75e1386d4ef8630a179e033fccf
Beck <1008887@student.sae.edu.au>
2018-08-13 13:56:27 +1000

    added some placeholder audio found a solution to the "play audio while moving" problem

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,9 @@ public class PlayerMovement : MonoBehaviour {
 
     public GameObject midPoint;
 
+    private bool isPlaying = false;
+    public AudioSource telescopeSound;
+
     private void OnEnable()
     {
         //Checking when to reset the position.
@@ -31,6 +34,37 @@ public class PlayerMovement : MonoBehaviour {
 
         rb2d.AddForce(movement * speed);
     }
+
+    //this stuff works, but it's not really ideal.
+    //if I could just play it "while moving", that would be great.
+    private void Update()
+    {
+        //add that bool so it doesn't start playing over and over
+        if (rb2d.velocity.magnitude >= 0.2f)
+        {
+            telescopeSound.Play();
+        }
+
+     ////playing a sound while a button is held down
+     //if (!isPlaying)
+     //{
+     //    if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+     //    {
+     //        isPlaying = true;
+     //        telescopeSound.Play();
+     //    }
+     //}
+     //
+     //if (isPlaying)
+     //{
+     //    if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+     //    {
+     //        isPlaying = false;
+     //        telescopeSound.Stop();
+     //    }
+     //}
+    }
+
 
     //Resetting the player's position on the center of the screen.
     public void ResetMovement ()
